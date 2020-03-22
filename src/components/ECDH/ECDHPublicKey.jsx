de25be5c4f7ca3860e4f1327bd5d3ebe936bab4a
lbeder <leonid@lbeder.com>
2020-03-22 02:02:53 +0200

    Add PoP footnote

--- src/components/ECDH/ECDHPublicKey.jsx
+++ src/components/ECDH/ECDHPublicKey.jsx
@@ -79,8 +79,9 @@ const ECDHPublicKey = () => {
               validations={[isPublicKey]}
               onChange={onChangeUpdateInput}
             />
-            <small className="form-text text-muted">66 characters long hexadecimal <strong>compressed</strong> public
-            key (1+32 bytes). The key should start with either 0x02 or 0x03.</small>
+            <small className="form-text text-muted">
+              66 characters long hexadecimal <strong>compressed</strong> public key (1+32 bytes). The key should start with either 0x02 or 0x03.
+            </small>
           </Col>
         </FormGroup>
 
@@ -147,6 +148,10 @@ const ECDHPublicKey = () => {
                 <li>The shared public key would be <strong><i>abG</i></strong>.</li>
                 <li>The shared private key would be <strong><i>ab</i></strong>.</li>
               </ul>
+            </small>
+
+            <small className="form-text text-muted">
+              Please make sure to verify the ownerships of the public keys using the  <strong>Proof of Possession</strong> method above to avoid <strong>DoS</strong> and <strong>Rouge Key Attacks</strong>.
             </small>
           </Col>
         </FormGroup>
