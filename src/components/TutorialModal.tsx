bd5ac868a67118adaa1ccc4d2425a4127b8f4f68
Leonid Beder <leonid.beder@gmail.com>
2020-04-09 23:05:39 +0300

    Add SMPC tutorial page

--- src/components/TutorialModal.tsx
+++ src/components/TutorialModal.tsx
@@ -66,6 +66,10 @@ const TutorialModal = (props: TutorialModalProps) => {
       case SLIDE_KEYS.EXTRACT_FROM_TRANSACTION:
         return 4;
 
+      case SLIDE_KEYS.SMPC_PRIVATE_KEY:
+      case SLIDE_KEYS.SMPC_PUBLIC_KEY:
+        return 5;
+
       default:
         return 0;
     }
@@ -296,6 +300,7 @@ const TutorialModal = (props: TutorialModalProps) => {
 
                 <div>
                   <h3>Public Key and Address Extraction</h3>
+
                   <p>
                     For your convenience, we have also provided the following tools to help you with public key
                     extraction and address conversion:
@@ -330,6 +335,82 @@ const TutorialModal = (props: TutorialModalProps) => {
                       prefix) and convert it to an address
                     </li>
                     <li>Extract a public key from a signed transaction and convert it to an address</li>
+                  </ul>
+
+                  <Button className="tutorial-next" onClick={onClickNext}>
+                    Next
+                  </Button>
+                </div>
+
+                <div>
+                  <h3>Secure Multi-party Computation (SMPC)</h3>
+
+                  <p>
+                    Even though both of the ECDH and the Aggregation key agreement protocols are cryptographically
+                    sound, they suffer from the following disadvantages:
+                  </p>
+                  <ul>
+                    <li>
+                      Public keys are exposed (and thus leaked) during the agreement phase, thus sacrificing some
+                      privacy aspects of the counterparties
+                    </li>
+                    <li>
+                      Private keys are exposed (and thus leaked) during the derivation/signing phase, thus effectively
+                      restricting the shared private key to one-time usage
+                    </li>
+                  </ul>
+
+                  <p>
+                    In the case when either higher level of privacy or recurring usage of the private key, an 
+                    <a
+                      href="https://en.wikipedia.org/wiki/Secure_multi-party_computation"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Secure Multi-party Computation (SMPC)
+                    </a>
+                     based{' '}
+                    <a
+                      href="https://en.wikipedia.org/wiki/Distributed_key_generation protocol"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Distributed Key Generation (DKG)
+                    </a>{' '}
+                    should be used instead.
+                  </p>
+
+                  <p className="form-text">
+                    We are considering adding the future versions DKG protocols based on the following researches:
+                  </p>
+                  <ul>
+                    <li>
+                      <a
+                        href="https://dl.acm.org/doi/10.1145/3243734.3243859"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Rosario Gennaro and Steven Goldfeder. 2018.{' '}
+                        <strong>Fast Multiparty Threshold ECDSA with Fast Trustless Setup.</strong> In Proceedings of
+                        the 2018 ACM SIGSAC Conference on Computer and Communications Security (CCS ’18). Association
+                        for Computing Machinery, New York, NY, USA, 1179–1194.
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://dl.acm.org/doi/10.1145/3243734.3243788"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Yehuda Lindell and Ariel Nof. 2018.{' '}
+                        <strong>
+                          Fast Secure Multiparty ECDSA with Practical Distributed Key Generation and Applications to
+                          Cryptocurrency Custody.
+                        </strong>{' '}
+                        In Proceedings of the 2018 ACM SIGSAC Conference on Computer and Communications Security (CCS
+                        ’18). Association for Computing Machinery, New York, NY, USA, 1837–1854.
+                      </a>
+                    </li>
                   </ul>
                 </div>
               </Slider>
